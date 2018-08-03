79abf679bac455b48f8aeaf2e5688f732e99ede8
pesong <codeturbo@gmail.com>
2018-08-03 20:07:37 +0800

    master branch use coco dateset

--- lib/datasets/dummy_datasets.py
+++ lib/datasets/dummy_datasets.py
@@ -28,58 +28,20 @@ from utils.collections import AttrDict
 def get_coco_dataset():
     """A dummy COCO dataset that includes only the 'classes' field."""
     ds = AttrDict()
-    # classes = [
-    #     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-    #     'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
-    #     'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
-    #     'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
-    #     'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-    #     'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-    #     'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
-    #     'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-    #     'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
-    #     'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
-    #     'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
-    #     'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
-    #     'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-    # ]
-
     classes = [
-        '__background__',
-        'building',
-        'wall',
-        'chair',
-        'slops',
-        'table',
-        'garbage'
-        'pack',
-        'potted plant',
-        'carpet',
-        'ladder',
-        'face',
-        'pole',
-        'traffic sign',
-        'billboard',
-        'traffic light',
-        'dustbin',
-        'fence',
-        'traffic_cone',
-        'road_pile',
-        'truck',
-        'bus',
-        'motorcycle',
-        'robot',
-        'rider',
-        'bicycle',
-        'person',
-        'car',
-        'sky',
-        'vegetation',
-        'pavement',
-        'lane markings',
-        'sidewalk',
-        'road'
+        '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+        'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+        'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+        'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
+        'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+        'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
+        'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+        'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+        'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+        'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+        'scissors', 'teddy bear', 'hair drier', 'toothbrush'
     ]
-
     ds.classes = {i: name for i, name in enumerate(classes)}
     return ds
